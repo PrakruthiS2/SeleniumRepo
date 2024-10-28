4ca8f7bca83371603aae4757a6a53b3c8c926f12
Prakruthi <prakruthisnadig@gmail.com>
2024-10-28 06:39:11 +0530

    added cart page

---
+++ src/main/java/seleniumSessions/CartPage.java
@@ -0,0 +1,13 @@
+package seleniumSessions;
+
+import org.openqa.selenium.By;
+
+public class CartPage {
+
+	By cartpage = By.id("cartpage");
+
+	public void getDemo() {
+		System.out.println("cart");
+	}
+
+}
